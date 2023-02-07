b076dcb76c8337688486a1bfd4bf5867cbac6b9e
Martin Helešic <martinhelesic@Martin-MacBook-Pro.local>
2023-02-07 20:54:56 +0100

    TimeTravel

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -11,12 +11,13 @@ class Game extends Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
 
     handleClick(i) {
-        const history = this.state.history
+        const history = this.state.history.slice(0, this.state.stepNumber + 1)
         const current = history[history.length - 1]
         const squares = current.squares.slice()
 
@@ -30,14 +31,32 @@ class Game extends Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        })
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         })
     }
 
     render() {
         const history = this.state.history
-        const current = history[history.length - 1]
+        const current = history[this.state.stepNumber]
         const winner = calculateWinner(current.squares)
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start'
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
         let status
 
         if (winner) {
@@ -47,18 +66,25 @@ class Game extends Component {
         }
 
         return (
-            <div className="game">
-                <div className="game-board">
-                    <Board 
-                        squares={current.squares}
-                        onClick={ (i) => this.handleClick(i)}
-                    />
+            <div>
+                <div className="game-play">
+                    <div>{status}</div>                    
+                    <div>History</div>
                 </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>        
+                <div className="game">
+                    
+                    <div className="game-board">
+                        <Board 
+                            squares={current.squares}
+                            onClick={ (i) => this.handleClick(i)}
+                            />
+                    </div>
+                        
+                    <div className="game-info">
+                        <ol>{moves}</ol>
+                    </div>
+                </div>        
+            </div>
         )
     }
 }
